a654f31cb407ade072d5f8112126f9dff58f586d
Zpub <lil.rhymes9@gmail.com>
2023-12-30 18:16:52 +0300

    Initial commit

---
+++ src/test/java/com/example/prepodov_net/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prepodov_net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
