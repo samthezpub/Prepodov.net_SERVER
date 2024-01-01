d2115e610eb54c3480cae8479a32ffcd5ce95207
dxrkfuture <dxrkfuture@gmail.com>
2024-01-01 23:54:37 +0300

    I_AM_A_TEAPOT на главной странице

--- src/main/java/com/example/prepodov_net/Controller/MainPageController.java
+++ src/main/java/com/example/prepodov_net/Controller/MainPageController.java
@@ -2,7 +2,9 @@ package com.example.prepodov_net.Controller;
 
 import com.example.prepodov_net.Services.Implementation.UserServiceImplementation;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +14,9 @@ public class MainPageController {
     private UserServiceImplementation userService;
 
     @GetMapping("/")
-    public String getMainPage(){
-        return "Продам гараж +7 (949) 352 - 56 - 43";
+    @ResponseStatus(value = HttpStatus.I_AM_A_TEAPOT)
+    public String getMainPage() {
+
+        return "Продам гараж +7 (949) 801 73 00";
     }
 }
