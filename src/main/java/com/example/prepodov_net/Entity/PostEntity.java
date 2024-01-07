0bbb0abe2a81f7b14bebbfa4d7102bb688e1cf4d
MrDrag0nXYT <147036304+MrDrag0nXYT@users.noreply.github.com>
2024-01-07 14:25:49 +0300

    formatted creation time for posts

--- src/main/java/com/example/prepodov_net/Entity/PostEntity.java
+++ src/main/java/com/example/prepodov_net/Entity/PostEntity.java
@@ -3,6 +3,9 @@ package com.example.prepodov_net.Entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Data
@@ -24,8 +27,38 @@ public class PostEntity extends Author {
     @Enumerated(EnumType.STRING)
     private AuthorType authorType;
 
+    @Column(name = "posted_at")
+    private LocalDateTime postedAt;
+
+    @Transient
+    private String postedAtFormatted;
+
 //    @OneToMany(fetch = FetchType.EAGER)
 //    private List<CommentEntity> comments;
+
+    public String getPostedAtFormatted() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime postTime = postedAt;
+
+        long secondsDiff = ChronoUnit.SECONDS.between(postTime, now);
+        long minutesDiff = ChronoUnit.MINUTES.between(postTime, now);
+        long hoursDiff = ChronoUnit.HOURS.between(postTime, now);
+
+        String postedAtFormatted;
+        if (secondsDiff < 5) {
+            postedAtFormatted = "только что";
+        } else if (secondsDiff < 60) {
+            postedAtFormatted = String.format("%d секунд назад", secondsDiff);
+        } else if (minutesDiff < 60) {
+            postedAtFormatted = String.format("%d минут назад", minutesDiff);
+        } else if (hoursDiff < 24) {
+            postedAtFormatted = String.format("%d часов назад", hoursDiff);
+        } else {
+            postedAtFormatted = postTime.format(DateTimeFormatter.ofPattern("HH:mm, dd MMMM, yyyy"));
+        }
+
+        return postedAtFormatted;
+    }
 
 }
 
